8f3df1232dde53bcadad55295aba635c88ba25f4
sarias12 <1638@holbertonschool.com>
2020-08-18 05:39:46 +0000

    Task 2 - distributes an archive to your web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,6 +3,7 @@
 from fabric.api import local, put, run, env
 from datetime import datetime
 from os.path import exists
+from fabric.network import disconnect_all
 
 env.hosts = ['35.237.150.134', '35.185.50.255']
 
@@ -26,18 +27,18 @@ def do_deploy(archive_path):
         return False
 
     put(archive_path, "/tmp/")
-    namefile = archive_path.split('/')[-1]
-    name = namefile.split('.')[0]  # name withouth extension.
+    fullname = archive_path.split('/')[-1]
+    name = fullname.split('.')[0]
     pathfile = "/data/web_static/releases/" + name
     run("mkdir -p " + pathfile)
-    run("tar -xzf /tmp/" + namefile + " -C " + pathfile)
-    run("rm /tmp/" + namefile)
+    run("tar -xzf /tmp/" + fullname + " -C " + pathfile)
+    run("rm /tmp/" + fullname)
 
     run("mv " + pathfile + "/web_static/* " + pathfile)
     run("rm -rf " + pathfile + "/web_static/")
     run("rm -rf /data/web_static/current")
     run("ln -s {} /data/web_static/current".format(pathfile))
-    
+
     disconnect_all()
-    
+
     return True
