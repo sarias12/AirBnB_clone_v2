c1e30e6688a4a591f1efb51ca0cc28ab3a4a59a0
sarias12 <1638@holbertonschool.com>
2020-09-02 02:57:23 +0000

    Task 4 - Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Modele For start a Flask web application
+"""
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display Text hello"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display Text"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Display with value of text variable"""""
+    return 'C %s' % escape(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """Display with value of text variable"""""
+    return 'Python %s' % escape(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Display with value of integer variable"""""
+    return '{:d} is a number'.format(n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
