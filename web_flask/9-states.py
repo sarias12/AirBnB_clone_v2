9a08456f9bc3f1b7ea511f3dd0dd2d5407098899
sarias12 <1638@holbertonschool.com>
2020-09-03 03:43:42 +0000

    Task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Modele For start a Flask web application
+"""
+from flask import Flask, escape, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states(id):
+    list = []
+    states = storage.all(State)
+    if id:
+        for state in states.values():
+            if state.id == id:
+                state_display = state
+        return render_template('9-states.html', state=state_display)
+    else:
+        for key, value in states.items():
+            list.append(value)
+        return render_template('9-states.html', list=list)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
